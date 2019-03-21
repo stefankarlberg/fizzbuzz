578b26b3f85afd22b29ca3255cc0da09741bdc80
stefan_karlberg <stefan.karlberg@smackit.se>
2019-03-21 19:44:57 +0100

    added more parameters

--- lib/fizz_buzz.rb
+++ lib/fizz_buzz.rb
@@ -1,10 +1,17 @@
 # lib/fizz_buzz.rb
 
-
 def fizz_buzz(number)
-    if number % 3 == 0
-      'fizz'
+    
+    if number % 15 == 0
+        "fizzbuzz"
+    
+    elsif number % 5 == 0 
+        'buzz'
+
+    elsif number % 3 == 0
+        'fizz'
     else
-      number
-    end 
-  end
+        number
+    end
+end
+
--- spec/fizz_buzz_spec.rb
+++ spec/fizz_buzz_spec.rb
@@ -2,19 +2,25 @@ require './lib/fizz_buzz'
 
 describe 'fizz_buzz' do
 
-    
-# spec/fizz_buzz_spec.rb
-
-it 'returns 1 if number is 1' do
-    expect(fizz_buzz(1)).to eq 1 
-end
-
 
 # spec/fizz_buzz_spec.rb
 
-it "returns 'fizz' if number is divisible by 3" do
-    expect(fizz_buzz(3)).to eq 'fizz'
-  end
+it "returns fizzbuzz if number is divisible by 15" do
+    expect(fizz_buzz(15)).to eq "fizzbuzz"
+end
+
+it 'returns "buzz" if number is divisible by 5' do
+    expect(fizz_buzz(5)).to eq "buzz"
+end
+
+
+it 'returns "fizz" if number is divisible by 3' do
+    expect(fizz_buzz(3)).to eq "fizz"
+end
+
+
+
 
 
 end
+
